9d494874da94ff7d9481c328da86ab13d4ba579c
Jakub Mazur <j.mazur.nl@gmail.com>
2024-09-25 02:35:59 +0200

    ✨ Set the simple main page

---
+++ components/ui/hero-section.tsx
@@ -0,0 +1,49 @@
+import { Button } from "@/components/ui/button";
+
+export default function HeroSectionGradientBackground() {
+  return (
+    <>
+      {/* Hero */}
+      <div className="">
+        {/* Gradients */}
+        <div
+          aria-hidden="true"
+          className="flex absolute -top-96 start-1/2 transform -translate-x-1/2"
+        >
+          <div className="bg-gradient-to-r from-background/50 to-background blur-3xl w-[25rem] h-[44rem] rotate-[-60deg] transform -translate-x-[10rem]" />
+          <div className="bg-gradient-to-tl blur-3xl w-[90rem] h-[50rem] rounded-full origin-top-left -rotate-12 -translate-x-[15rem] from-primary-foreground via-primary-foreground to-background" />
+        </div>
+        {/* End Gradients */}
+        <div className="relative z-10">
+          <div className="container py-10 lg:py-16">
+            <div className="max-w-2xl text-center mx-auto">
+              <p className="">Turn your music into art</p>
+              {/* Title */}
+              <div className="mt-5 max-w-2xl">
+                <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                  Albumfy
+                </h1>
+              </div>
+              {/* End Title */}
+              <div className="mt-5 max-w-3xl">
+                <p className="text-xl text-muted-foreground">
+                  Generate stunning posters from your favorite Spotify albums
+                  and tracks. Personalize and share your musical journey.
+                </p>
+              </div>
+              {/* Buttons */}
+              <div className="mt-8 gap-3 flex justify-center">
+                <Button size={"lg"}>Get started</Button>
+                <Button size={"lg"} variant={"outline"}>
+                  Learn more
+                </Button>
+              </div>
+              {/* End Buttons */}
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* End Hero */}
+    </>
+  );
+}
